b439ecdc1b284d33bb3a8d24bd2a539ad2366754
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2021-02-24 20:00:37 +0800

    Create delete.js

---
+++ src/delete.js
@@ -0,0 +1,46 @@
+function viewOwn(userID) { 
+  var rangeValues = requestRange();
+  var count = 0;
+  var keyboard = [];
+
+  for (i = 0; i < requestLastRow() - 1; i++) {
+    var refId = rangeValues[i][0];
+    var req = requestInfo(refId);    
+    if (req.userId === userID && req.status === "Available") {
+        keyboard[count] = [
+            {
+              text: req.request + " (" + req.credits + " cr) " + '\n '+ req.time.slice(0, -2) + ", " + req.date.slice(0, -2),
+              callback_data: 'cancel-' + i,
+            },
+        ];
+        count++;
+    }
+  }
+
+  if (count === 0) {
+    return false;
+  } else {
+    return {inline_keyboard: keyboard};
+  }
+}
+
+function cancelRequest(row_data, userID) {
+    // some gross programming here, edit : its beautiful
+    var refId = parseInt(row_data) + 1;
+    var req = requestInfo(refId);
+    
+    var user = userInfo(userID)
+    var total_credits = user.total_credits;
+    
+    if (req.userId === userID && req.status === 'Available') {
+      setRequestStatus(refId, "Cancelled");
+      var pending = req.pending;
+      var newCredits = total_credits + pending;
+      setRequestPending(refId, 0);
+      setUserCredits(userID, newCredits);
+      
+      return 'Request cancelled!';
+    } else {
+      return 'You have no active requests!';
+    }
+}
